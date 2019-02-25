3889f739d9df3602b4d4d193a48bd061ee5397ea
jhlee <jhlee.lubobo128@gmail.com>
2019-02-25 10:48:53 +0900

    FrontController(MVC)

--- src/main/java/kr/or/ddit/servlet/FrontController.java
+++ src/main/java/kr/or/ddit/servlet/FrontController.java
@@ -11,25 +11,47 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-// userList.do,rangersList.do ==>
+import kr.or.ddit.front.CommonController;
+import kr.or.ddit.front.HandlerInvoker;
+import kr.or.ddit.front.HandlerMapper;
+import kr.or.ddit.front.ViewResolver;
+
+// ex) userList.do rangersList.do 
 @WebServlet("*.do")
 public class FrontController extends HttpServlet{
-
-	private Logger logger = LoggerFactory
-			.getLogger(FrontController.class);
+	
+	private Logger logger = LoggerFactory.getLogger(FrontController.class);
+	
+	private HandlerMapper handlerMapper;
+	private HandlerInvoker handlerInvoker;
+	private ViewResolver viewResolver;
 	
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		logger.debug("frontController doGet : {}",req.getRequestURI());
-		
-		
+	public void init() throws ServletException {
+		handlerMapper = new HandlerMapper();
+		handlerInvoker = new HandlerInvoker();
+		viewResolver = new ViewResolver();
 	}
 
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		logger.debug("frontController doGet : {}", req.getRequestURI());
+		
+		//uri 요청을 처리해줄 컨트롤러 확인
+		CommonController controller = handlerMapper.getController(req.getRequestURI());
+		
+		//컨트롤러 실행
+		String viewInfo = handlerInvoker.invoke(req, resp, controller);
+		
+		//viewResolver를 통한 응답 생성
+		viewResolver.viewResolve(req, resp,viewInfo);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		doGet(req, resp);
 	}
+
 	
 }
